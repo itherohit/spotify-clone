7aa11af790c0a754b92926b443b4eea03177c92c
itherohit <rohitshan751@gmail.com>
2021-05-03 20:58:56 +0530

    Login and ContextAPI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { useEffect, useState, useStateValue } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
 import SpotifyWebApi from "spotify-web-api-js";
 import { getTokenFromUrl } from "./components/Spotify";
 import Login from "./components/Login";
-import Player from "./components/Player"
+import Player from "./components/Player";
+import { useStateValue } from './components/StateProvider';
 
 const spotify = new SpotifyWebApi();
 
 function App() {
-
-  const [token, setToken] = useState();
+  const [{ user, token },dispatch] = useStateValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
     const _token = hash.access_token;
     window.location.hash = "";
     if(_token){
-      setToken(_token);
+      dispatch({
+        type: 'SET_TOKEN',
+        token: _token
+      })
       spotify.setAccessToken(_token);
       spotify.getMe().then(user => {
-        console.log(user);
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        });
       })
     }
   }, [token]);
+
+  console.log(user);
 
   return (
     <div className="app">
