795042a4e13a892ef52a292f2a692dbf7b43e728
itherohit <rohitshan751@gmail.com>
2021-07-18 20:43:53 +0530

    Fixed errors

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -1,7 +1,8 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const clientId = "cff76213089a4e228206c8bf120d67cb";
-const redirectUri = "http://localhost:3000/";
+const clientId = process.env.REACT_APP_CLIENT_ID;
+const redirectUri = process.env.REACT_APP_REDIRECT_URL;
+console.log(process.env.CLIENT_ID);
 const scopes = [
   "user-read-currently-playing",
   "user-read-recently-played",
